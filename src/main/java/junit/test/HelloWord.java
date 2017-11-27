c770365859f515fc79f9edd4b49134ece801fcc0
root <root@atguigu.com>
2017-11-27 19:58:51 +0800

    ff

--- src/main/java/junit/test/HelloWord.java
+++ src/main/java/junit/test/HelloWord.java
@@ -3,7 +3,7 @@ package junit.test;
 public class HelloWord {
  
 	public static void main(String[] args) {
-		System.out.println("What do you want for me ?");
-		System.out.println("666");
+		System.out.println("What do you want for me ?"+1);
+		
 	}
 }
